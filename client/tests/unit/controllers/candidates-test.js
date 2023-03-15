c25914b1537319d2b560d96d4639807caa8c4cd9
Kevin Boucher <KBoucher@idauto.net>
2023-03-15 12:11:40 -0500

    Run linters with `--fix` option (would like to change some of these rules)

--- client/tests/unit/controllers/candidates-test.js
+++ client/tests/unit/controllers/candidates-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Controller | candidates', function(hooks) {
+module('Unit | Controller | candidates', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let controller = this.owner.lookup('controller:candidates');
     assert.ok(controller);
   });
